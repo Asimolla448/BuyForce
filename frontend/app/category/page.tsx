134aaa20417be2a22ef615b7884e591446fdecb3
Asimolla448 <asimolla448@gmail.com>
2026-01-04 22:41:37 +0200

    Delete frontend directory

--- frontend/app/category/page.tsx
+++
@@ -1,6 +0,0 @@
-
-export default function Category(){
-    return(
-        <div>asdsadadsa</div>
-    )
-};
--- frontend/app/myGroup/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function MyGroup() {
-  return (
-    <div>
-      M
-    </div>
-  )
-}
